cf4aa797be34d62a21cbb94f22c2fe89604a9f0e
Michelle Goulbourne Robinson <michellegrobinson1@gmail.com>
2025-08-27 10:48:10 -0400

    chore: install R deps to user library (Windows-safe)

--- requirements.R
+++ requirements.R
@@ -1,5 +1,19 @@
-# Install minimal deps for testing and benchmarking
-pkgs <- c("testthat", "microbenchmark")
-to_install <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
-if (length(to_install)) install.packages(to_install, repos="https://cloud.r-project.org")
-message("Dependencies ensured: ", paste(pkgs, collapse=", "))
+# Windows-safe requirements installer: always use user library
+lib <- Sys.getenv("R_LIBS_USER")
+if (lib == "") {
+  user <- Sys.getenv(ifelse(.Platform$OS.type == "windows","USERPROFILE","HOME"))
+  ver  <- paste(R.version$major, R.version$minor, sep=".")
+  lib  <- if (.Platform$OS.type == "windows")
+            file.path(user, "AppData/Local/R/win-library", ver)
+          else
+            file.path(user, "R", ver)
+}
+dir.create(lib, recursive = TRUE, showWarnings = FALSE)
+.libPaths(c(lib, .libPaths()))
+
+pkgs <- c("testthat","microbenchmark","yaml")
+to_install <- setdiff(pkgs, rownames(installed.packages(lib.loc = .libPaths())))
+if (length(to_install))
+  install.packages(to_install, lib = lib, repos = "https://cloud.r-project.org")
+
+message("Using libs: ", paste(.libPaths(), collapse = " | "))
